8844ab7f79b452efd07ae97739a9758b1cae17cd
Marcelo Gomes <marcelo@mgsistemas.com.br>
2023-04-05 16:30:21 -0300

    Corrige importacao "socket.io-client"

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,7 +34,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Echo from 'laravel-echo';
 
-window.io = require('soket.io-client');
+window.io = require('socket.io-client');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
